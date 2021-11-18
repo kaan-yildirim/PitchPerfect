09536d07b00a798f5dba5a1a266c5f2ff92146ab
TURKCELL\tckayildirim <kaan.yildirim@turkcell.com.tr>
2021-11-18 12:14:39 +0300

    Add record elements

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func recordButtonPressed(_ sender: Any) {
+        recordLabel.text = "Record in progress"
+    }
 
+    @IBAction func stopRecordButtonPressed(_ sender: Any) {
+    }
 }
 
